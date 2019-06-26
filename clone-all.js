8f9deb402e8b3f175468cf2e09b0a2f95a851041
lrrinaudo <lrrinaudo@gmail.com>
2019-06-25 21:09:07 -0300

    docker-compose

--- clone-all.js
+++ clone-all.js
@@ -7,5 +7,5 @@ if (!fs.existsSync(dir)){
 
 const { execSync } = require("child_process");
 
-execSync("git clone https://github.com/PracticaDS/2019-todos-backend cloned-repos/todos-backend")
-execSync("git clone https://github.com/pablitar/2019-react-redux-todo-list cloned-repos/todos-frontend")
+execSync("git clone -b desarrollo https://github.com/PracticaDS/pdes-tp-backend-grupo1.git cloned-repos/backend")
+execSync("git clone -b desarrollo https://github.com/PracticaDS/pdes-tp-grupo1.git cloned-repos/frontend")
